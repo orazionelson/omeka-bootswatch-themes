1151172bc9fae331f5571eb6de29ca624fe29308
Steph <36681924+stephherbers@users.noreply.github.com>
2019-03-11 18:40:02 -0500

    fixed navbar

--- common/headerMain.php
+++ common/headerMain.php
@@ -41,16 +41,31 @@
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
-    <nav class="navbar navbar-default navbar-fixed-top"><!-- navbar-fixed-top -->
-      <div class="container">
-        <div class="navbar-header">
-          <?php echo bs_link_logo_to_navbar(); ?>
+      <!-- <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <?php echo bs_link_logo_to_navbar(); ?>
+          </div>
+          <div class="navbar-collapse collapse" id="navbar-main">
+            <?php echo public_nav_main_bootstrap(); ?>
+            <?php echo search_form(array('show_advanced' => false, 'form_attributes'=>array('id'=>'navbar-search', 'class'=>'navbar-form navbar-right'))); ?>
+          </div>
         </div>
-        <div class="navbar-collapse collapse" id="navbar-main">
-          <?php echo public_nav_main_bootstrap(); ?>
-          <?php echo search_form(array('show_advanced' => false, 'form_attributes'=>array('id'=>'navbar-search', 'class'=>'navbar-form navbar-right'))); ?>
-        </div>
-      </div>
-    </nav>
+      </nav> -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><?php echo option('site_title') ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo html_escape(url('items')); ?>">Items</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo html_escape(url('collections')); ?>">Collections</a>
+            </li>
 
-    
+          </ul>
+        </div>
+      </nav>
