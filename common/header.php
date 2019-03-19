19f2a83cbf54e6672d58564c1e2fe6d978af060e
apjmason <austin.pj.mason@gmail.com>
2019-03-19 17:31:46 -0500

    Copied changes from headermain to header

--- common/header.php
+++ common/header.php
@@ -43,22 +43,16 @@
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#"><?php echo bs_link_logo_to_navbar(); ?></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo html_escape(url('items')); ?>">Items</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo html_escape(url('collections')); ?>">Collections</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <?php echo bs_link_logo_to_navbar(); ?>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <?php echo public_nav_main_bootstrap(); ?>
+            <?php echo search_form(array('show_advanced' => false, 'form_attributes'=>array('id'=>'navbar-search', 'class'=>'navbar-form navbar-right'))); ?>
+        </div>
+      </nav>
     <main id="content">
        <div class="container-fluid">
           <?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
